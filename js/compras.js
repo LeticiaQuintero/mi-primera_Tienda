4f0c7a5f5b7128cc3bbfa73ab2ba4db526d644f8
lety <glolet24@gmail.com>
2021-11-08 18:18:21 -0600

    se agregaron precios y unidades en js

---
+++ js/compras.js
@@ -0,0 +1,23 @@
+/*/solicitar los datos con un promt
+var costo = Number (prompt("precio del producto")//el prompt arroja strings)
+
+var unidad = Number (prompt("precio por unidad"))
+
+var TOTAL = costo * unidad
+alert(TOTAL)*/
+
+var preciopastel30 = Number(prompt("precio del pastel para 30 personas"))
+var unidades = Number(prompt("numero de pasteles"))
+var totalpastel30 = preciopastel30 * unidades
+
+var preciopastel20 = Number(prompt("precio del pastel para 20 personas"))
+var unidad = Number(prompt("numero de pasteles"))
+var totalpastel20 = preciopastel20 * unidad
+
+var preciopastel15 = Number(prompt("precio del pastel para 15 personas"))
+var Unidaeds = Number (prompt("numero de pasteles"))
+var totalpastel15 = preciopastel15 * Unidaeds
+
+var total = totalpastel15 + totalpastel20 +totalpastel30
+alert(total)
+//seleccionar el div de html que se va a imprimir//
